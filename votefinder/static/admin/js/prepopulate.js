fc5cdd96b93f554ad1ee24c87003836cb18ee1a0
Ryan Magley <ryan.magley@gmail.com>
2023-06-23 16:17:00 -0500

    running collectstatic added these files, part of the Django admin panel I assume

--- votefinder/static/admin/js/prepopulate.js
+++ votefinder/static/admin/js/prepopulate.js
@@ -1,34 +1,43 @@
-(function ($) {
-    $.fn.prepopulate = function (dependencies, maxLength) {
+/*global URLify*/
+'use strict';
+{
+    const $ = django.jQuery;
+    $.fn.prepopulate = function(dependencies, maxLength, allowUnicode) {
         /*
             Depends on urlify.js
             Populates a selected field with the values of the dependent fields,
-            URLifies and shortens the string. 
-            dependencies - selected jQuery object of dependent fields
-            maxLength - maximum length of the URLify'd string 
+            URLifies and shortens the string.
+            dependencies - array of dependent fields ids
+            maxLength - maximum length of the URLify'd string
+            allowUnicode - Unicode support of the URLify'd string
         */
-        return this.each(function () {
-            var field = $(this);
+        return this.each(function() {
+            const prepopulatedField = $(this);
 
-            field.data('_changed', false);
-            field.change(function () {
-                field.data('_changed', true);
-            });
+            const populate = function() {
+                // Bail if the field's value has been changed by the user
+                if (prepopulatedField.data('_changed')) {
+                    return;
+                }
 
-            var populate = function () {
-                // Bail if the fields value has changed
-                if (field.data('_changed') == true) return;
-
-                var values = [];
-                dependencies.each(function () {
-                    if ($(this).val().length > 0) {
-                        values.push($(this).val());
+                const values = [];
+                $.each(dependencies, function(i, field) {
+                    field = $(field);
+                    if (field.val().length > 0) {
+                        values.push(field.val());
                     }
                 });
-                field.val(URLify(values.join(' '), maxLength));
+                prepopulatedField.val(URLify(values.join(' '), maxLength, allowUnicode));
             };
 
-            dependencies.keyup(populate).change(populate).focus(populate);
+            prepopulatedField.data('_changed', false);
+            prepopulatedField.on('change', function() {
+                prepopulatedField.data('_changed', true);
+            });
+
+            if (!prepopulatedField.val()) {
+                $(dependencies.join(',')).on('keyup change focus', populate);
+            }
         });
     };
-})(django.jQuery);
+}
